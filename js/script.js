1ae146cd97f9d24ed9c7947cdcf36b9a039c478e
Denise Pingiori <pingiorid@gmail.com>
2024-07-01 18:23:05 +0200

    [upd]upd array and confronted mail

--- js/script.js
+++ js/script.js
@@ -1,6 +1,21 @@
 //creare array di email su cui effettuare ilù controllo
-let email = ['fabrizio@mail.com','denise@mail-com', 'giulia@mail.com', 'luca@mail.com'];
+let emails = ['fabrizio@mail.com','denise@mail-com', 'giulia@mail.com', 'luca@mail.com'];
 
 //recupero pulsante dal dom
 const btn = document.getElementById('check'); 
 
+// aggiungo evento click al pulsante
+btn.addEventListener('click', function(){
+
+    //recupero valore campo input
+    let user_mail= document.getElementById('email').value;
+    //console.log(user_email); per vedere se funziona
+
+    //ciclo l'array
+    for(let i=0; i<emails.length; i++) {
+        //confronto mail inserita dall'utente con quelle attualmente nell'array
+        if(emails[i] == user_mail){   //emails[i]: elemento zero dell'array
+           console.log('benvenuto');
+        }
+    }
+})
